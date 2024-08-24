d23f027cfa0bc2e5e99403715bfc4cc49ea9c7db
g-WSL <gsakore22@gmail.com>
2024-08-24 17:13:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import searchIcon from './images/search.svg'
+import MovieCard from './MovieCard';
 
-function App() {
+const BASE_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=929925c6';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchPhrase, setSearchPhrase] = useState('');
+  const [isShrunk, setIsShrunk] = useState(false);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${BASE_URL}&s=${title}`)
+
+    const data = await response.json();
+    setMovies(data.Search);
+  }
+
+  const handleScroll = () => {
+    const scrollTop = window.scrollY;
+    setIsShrunk(scrollTop > 50);
+  };
+
+  useEffect(() => {
+    // searchMovies('');
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className={`header ${isShrunk ? 'shrink' : ''}`}>
+        <div>
+          <h1 className='title'>MovieDom</h1>
+        </div>
+        <panel className='search'>
+          <input
+            placeholder='Search for a movie'
+            value={searchPhrase}
+            onChange={(e) => setSearchPhrase(e.target.value)}
+          />
+          <img
+            src={searchIcon}
+            alt='search'
+            onClick={(e) => searchMovies(searchPhrase)}
+          />
+        </panel>
+      </div>
+      <div className='body'>
+        {
+          movies?.length > 0
+          ? (
+            <div className='container'>
+              {movies.map((movie) => (
+                <MovieCard movie={movie} />
+              ))}
+            </div>
+          ) : (
+            <div className='container'>
+              <h2>No movies found</h2>
+            </div>
+          )
+        }
+      </div>
     </div>
   );
 }
